8ed15a6cf035d2669ad743da9a3cdbaff0f23547
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-08 16:30:15 +0900

    ggg

--- Cccc/HelloC.c
+++ Cccc/HelloC.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main (void){
-    printf("Hello world\n");
+
+
+    //2. 개인정보 입력
+    int age = 12;
+    printf("%d \n", age);
     return 0;
+
+
+
+
+
+
+    //1. 안녕출력
+    // printf("Hello world\n");
+    // return 0;
 }
