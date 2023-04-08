6425335a61e0f994b0125aa5b611e9ef7b03ea07
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-04-08 16:37:04 +0900

    ...

--- Cccc/HelloC.c
+++ Cccc/HelloC.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main (void){
 
+    //3. 실수형 변순
+
+    float y = 46.5f; //float로 선언한 변수의 값에는 f가 붙어야 함
+    printf("%f\n",y);
+
 
     //2. 개인정보 입력
-    int age = 12;
-    printf("%d \n", age);
-    return 0;
-
-
-
+    // int age = 12;
+    // printf("%d \n", age);
+    // age = 13; // 선언한 변수의 내용물 바꿀 때 int 같은거 쓰면 충돌 생김. 선언한거 다시 선언하지만
+    // printf("%d \n", age);
+    // return 0;
 
 
 
